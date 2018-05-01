cc54b397f174be4baca3e465ef4002feba16a093
Sinem Hasırcıoğlu <02150001068@ogr.inonu.edu.tr>
2018-05-02 00:40:32 +0300

    Created loginFilter

---
+++ src/java/utility/LoginFilter.java
@@ -0,0 +1,49 @@
+package utility;
+
+import entities.Users;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sinem
+ */
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        String url = req.getRequestURI();        
+        Users u = (Users) req.getSession().getAttribute("valid_user");
+        
+        if (u == null) {
+            if(url.contains("admin") || url.contains("logout")) {
+                
+            }
+            
+        } else {
+            
+        }      
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
