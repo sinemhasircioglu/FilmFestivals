c578aec0714bee61d96cce4b02518bb98952120d
Sinem Hasırcıoğlu <02150001068@ogr.inonu.edu.tr>
2018-05-09 22:10:37 +0300

    added NavigationController

---
+++ src/java/utility/NavigationController.java
@@ -0,0 +1,22 @@
+package utility;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author sinem
+ */
+@Named(value="navigationController")
+@RequestScoped
+public class NavigationController implements Serializable{
+    
+    public String goToFrontendPage(String page) {
+        return "/frontend/"+page+"?faces-redirect=true";
+    }    
+    
+    public String goToAdminPage(String page) {
+        return "/admin/"+page+"?faces-redirect=true";
+    } 
+}
